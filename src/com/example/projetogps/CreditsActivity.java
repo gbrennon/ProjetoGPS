d8da42255482e27c24bf7ba5e1de2c5106530ce7
Gláuber Brennon <glauberbrennon@gmail.com>
2014-11-26 01:21:00 -0200

    Credits Activity added

---
+++ src/com/example/projetogps/CreditsActivity.java
@@ -0,0 +1,11 @@
+package com.example.projetogps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreditsActivity extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
